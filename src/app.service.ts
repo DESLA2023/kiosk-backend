40cb585b51ee5546097c4843ec7816c4aaabe0e2
이호연 <ho991217@kakao.com>
2023-06-01 15:00:39 +0900

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { message: string; status: number; data: null } {
-    return { message: 'Hello World!', status: 200, data: null };
+  getHello(): string {
+    return 'test string!';
   }
 }
